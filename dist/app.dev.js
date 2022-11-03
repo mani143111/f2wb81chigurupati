42714c946989467339b10f4a4d9c63282272f558
mani143111 <112142948+mani143111@users.noreply.github.com>
2022-11-02 21:19:25 -0500

    added

--- dist/app.dev.js
+++ dist/app.dev.js
@@ -14,6 +14,8 @@ var indexRouter = require('./routes/index');
 
 var usersRouter = require('./routes/users');
 
+var AppleRouter = require('./routes/Apple');
+
 var app = express(); // view engine setup
 
 app.set('views', path.join(__dirname, 'views'));
@@ -27,7 +29,7 @@ app.use(cookieParser());
 app.use(express["static"](path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/Apple', oatRouter); // catch 404 and forward to error handler
+app.use('/Apple', AppleRouter); // catch 404 and forward to error handler
 
 app.use(function (req, res, next) {
   next(createError(404));
